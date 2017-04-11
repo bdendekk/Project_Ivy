db372805e44db8a0058b967dead4b396e98a55db
bdendekk <benjamin.den_dekker@insa-cvl.fr>
2017-04-11 22:17:15 +0200

    ajout de la recherche du max dans data.csv

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -9,6 +9,16 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+        
+        //parcours
+	for (int i=0; i<myEntries.size(); i++){
+		System.out.println("Nombre lu : "+i);
+		if(myEntries.get(i) > monmax){
+			monmax = myEntries.get(i);
+		}
+	System.out.println("le maximum est : "+monmax);
+	}
+
     }
 }
         
